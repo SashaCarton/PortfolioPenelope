ed39349f7a3941d8a95700c55d92aaf97d050817
SashaCarton <163831379+SashaCarton@users.noreply.github.com>
2025-05-04 15:12:02 +0200

    fix: Mettre à jour la politique de sécurité et les configurations CORS

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,17 +1,39 @@
 module.exports = [
+  'strapi::logger',
   'strapi::errors',
 
   {
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
-        useDefaults: true,
         directives: {
-          'frame-ancestors': [
+          "default-src": ["'self'"],
+          "script-src": ["'self'", "'unsafe-inline'"],
+          "style-src": ["'self'", "'unsafe-inline'", "https:"],
+          "img-src": ["'self'", "data:", "blob:"],
+          "media-src": ["'self'", "data:", "blob:"],
+          "connect-src": [
             "'self'",
-            'https://api.penelopeletienne.ovh',
-            'https://penelopeletienne.ovh',
-            'http://localhost:5173',
+            "https:",
+            "http:",
+            "ws:",
+            "http://penelopeletienne.ovh",
+            "https://penelopeletienne.ovh",
+            "http://api.penelopeletienne.ovh",
+            "https://api.penelopeletienne.ovh",
+            "http://localhost:5173",
+          ],
+          "font-src": ["'self'", "https:", "data:"],
+          "object-src": ["'none'"],
+          "base-uri": ["'self'"],
+          "form-action": ["'self'"],
+          "script-src-attr": ["'none'"],
+          "frame-ancestors": [
+            "'self'",
+            "https://penelopeletienne.ovh",
+            "http://penelopeletienne.ovh",
+            "http://localhost:5173",
+            "https://api.penelopeletienne.ovh"
           ],
         },
       },
@@ -22,24 +44,18 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       origin: [
-        'https://api.penelopeletienne.ovh',
+        'http://penelopeletienne.ovh',
         'https://penelopeletienne.ovh',
         'http://localhost:5173',
+        'https://api.penelopeletienne.ovh',
       ],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-      headers: [
-        'Content-Type',
-        'Authorization',
-        'Origin',
-        'Accept',
-        'X-Requested-With',
-      ],
-      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      keepHeaderOnError: false,
     },
   },
 
   'strapi::poweredBy',
-  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
