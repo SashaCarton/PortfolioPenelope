79c1cabf4589fb296d4c72629358438d7781aaba
Penny0507 <penelope.letienne@ensaama.net>
2026-03-06 18:16:30 +0100

    pourquoi ça marche pas wallah????

--- frontend/src/views/ProjectDetails.vue
+++ frontend/src/views/ProjectDetails.vue
@@ -312,7 +312,7 @@ p {
   flex-wrap: wrap;
   gap: 1rem;
   width: 100%;
-  margin-top: 1.75rem; /* espace supplémentaire entre description et médias */
+  margin-top: 1.75rem; 
 }
 
 .project-media img {
